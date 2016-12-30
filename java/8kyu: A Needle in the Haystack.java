2a0beb5a5b63a28ff8012dc9ea10347e78aa9bec
Brennan Butler <bbutler522@gmail.com>
2016-12-30 10:16:41 -0500

    Added question description

--- java/8kyu: A Needle in the Haystack.java
+++ java/8kyu: A Needle in the Haystack.java
@@ -10,3 +10,20 @@ public class Kata {
     return "found the needle at position " + position;
   }
 }
+
+/*
+Can you find the needle in the haystack?
+
+Write a function findNeedle() that takes an array full of junk but containing one "needle"
+
+After your function finds the needle it should return a message (as a string) that says:
+
+"found the needle at position " plus the index it found the needle
+
+So
+
+findNeedle(new Object[] {"hay", "junk", "hay", "hay", "moreJunk", "needle", "randomJunk"})
+should return
+
+"found the needle at position 5"
+*/
